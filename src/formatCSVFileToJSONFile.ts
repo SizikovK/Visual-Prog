c8416d7442641b8b04c9c78c2b262ec309ea599c
Konstantin Sizikov <sizikovopop@mail.ru>
2026-03-05 10:50:01 +0700

    formatCSVFileToJSONFile done

--- src/formatCSVFileToJSONFile.ts
+++ src/formatCSVFileToJSONFile.ts
@@ -1,5 +1,18 @@
 import { readFile, writeFile } from 'node:fs/promises';
+import { csvToJSON } from './csvToJSON';
 
-function formatCSVFileToJSONFile(input: string, output: string, delimiter: string): Promise<void> {
+export async function formatCSVFileToJSONFile(input: string, output: string, delimiter: string): Promise<void> {
+    try {
+        
+        const csvstring = await readFile(input, 'utf-8');
 
+        const lines = csvstring.trim().split('\n');
+
+        const result = csvToJSON(lines, delimiter);
+
+        await writeFile(output, JSON.stringify(result, null, 2));
+    }
+    catch (error) {
+        throw new Error(`Ошибка обработки файла: ${error}`);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { csvToJSON } from "./csvToJSON";
+import { formatCSVFileToJSONFile } from "./formatCSVFileToJSONFile";
 
-let res = csvToJSON(["p1;p2;p3;p4", "1;A;b;c", "2;B;v;"], ';');
-
+let res = csvToJSON(["p1;p2;p3;p4", "1;A;b;c", "2;B;v;a"], ';');
 console.log(res);
+
+formatCSVFileToJSONFile('src/data/examp.csv', 'src/data/ex.json', ',');
