a7603afb59abf35f5acdc86e19c4c5c13c460ab9
Konstantin Sizikov <sizikovopop@mail.ru>
2026-03-17 11:05:09 +0700

    Types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+type DeepReadonly<T> = {
+    readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K]; 
+};
+
+export type PickedByType<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
+
+export type EventHandlers<T> = {
+  [K in keyof T as `on${Capitalize<string & K>}`]?: T[K];
+};
